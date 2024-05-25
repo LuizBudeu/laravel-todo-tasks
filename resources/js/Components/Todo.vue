1fc944b621b6063233e76b2d0002cd808a243f5b
Luiz Budeu <lugui@usp.br>
2024-05-25 00:05:46 -0300

    initial commit laravel breeze vue

---
+++ resources/js/Components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodo" placeholder="Enter a new todo" />
+        <button style="padding-left: 10px" @click="addTodo">Add Todo</button>
+
+        <ul>
+            <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodo: "",
+            todos: [],
+        };
+    },
+    methods: {
+        addTodo() {
+            this.todos.push(this.newTodo);
+            this.newTodo = "";
+        },
+    },
+};
+</script>
